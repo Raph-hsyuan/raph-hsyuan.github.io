9f71a2318090e42213740b2fe0fb784a657e031a
Raph-hsyuan <nauynehs_h@hotmail.com>
2020-05-15 17:55:58 +0200

    Site updated: 2020-05-15 17:55:58

--- js/matery.js
+++ js/matery.js
@@ -134,10 +134,8 @@ $(function () {
         var curWwwPath=window.document.location.href;
         var pathName=window.document.location.pathname;      
         var pos=curWwwPath.indexOf(pathName);            
-        var localhostPaht=curWwwPath.substring(0,pos);           
-        var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);      
-        var realPath=localhostPaht+projectName;      
-        return realPath
+        var localhostPaht=curWwwPath.substring(0,pos);                 
+        return localhostPaht
     }
 
     /*监听滚动条位置*/
